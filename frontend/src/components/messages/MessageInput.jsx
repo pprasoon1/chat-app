310b8f9f792ede79937d735453ef74b1c2599361
Pranay Prasoon <159932934+pprasoon1@users.noreply.github.com>
2024-09-26 00:01:40 +0530

    ui part

---
+++ frontend/src/components/messages/MessageInput.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {BsSend} from 'react-icons/bs'
+
+const MessageInput = () => {
+  return (
+    <form className='px-4 my-3'>
+        <div className='w-full relative'>
+    <input type="text" placeholder="Enter message"
+     className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+    />
+    <button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
+        <BsSend />
+    </button>
+        </div>
+    </form>
+  )
+}
+
+export default MessageInput
+
+
+
+// const MessageInput = () => {
+//     return (
+//       <form className='px-4 my-3'>
+//           <div className='w-full'>
+//       <input type="text" placeholder="Enter message"
+//        className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+//       />
+//       <button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
+//           <BsSend />
+//       </button>
+//           </div>
+//       </form>
+//     )
+//   }
+  
---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {BiLogOut} from 'react-icons/bi'
+const LogoutButton = () => {
+  return (
+    <div className='mt-auto '>
+        <BiLogOut className='w-6 h-6 text-2xl text-white hover:text-sky-300 cursor-pointer'/>
+    </div>
+  )
+}
+
+export default LogoutButton
