8a85519bdbd82415ccf956bcf4f5edfe32878d22
Pranay Prasoon <159932934+pprasoon1@users.noreply.github.com>
2024-10-07 00:03:40 +0530

    .

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,19 @@
+import mongoose from "mongoose";
+
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGODB_URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 30000, // 30 seconds timeout
+            serverSelectionTimeoutMS: 30000,
         });
         console.log("connected to database");
+        
+
     } catch (error) {
-        console.log("error connecting to database", error.message);
+        console.log("error connecting to database",error.message)
+        
     }
-};
+}
+
+export default connectToMongoDB;
